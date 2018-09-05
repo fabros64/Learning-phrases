92e24e9431394ea1dd1324481efbb13c499ccba4
fabros <fabros@op.pl>
2018-09-05 14:02:49 +0200

    Szkielet
    Podstawy

---
+++ Zwroty/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zwroty
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            btnOtworz.IsEnabled = true;
+            btnUsun.IsEnabled = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NowaBaza nb = new NowaBaza(btnNowa, btnTEST);
+            nb.Left = this.Left + this.Width - nb.Width - (this.Width - nb.Width)/2;
+            nb.Top = this.Top + this.Height - nb.Height - (this.Height - nb.Height)/2;
+            nb.Show();
+
+            btnNowa.IsEnabled = false;
+            btnTEST.IsEnabled = false;
+            btnOtworz.IsEnabled = false;
+            btnUsun.IsEnabled = false;
+
+           
+        }
+
+        
+    }
+}
---
+++ Zwroty/NowaBaza.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Zwroty
+{
+    /// <summary>
+    /// Logika interakcji dla klasy NowaBaza.xaml
+    /// </summary>
+    public partial class NowaBaza : Window
+    {
+        Button btnN1, btnN2;
+        public NowaBaza(Button btn1, Button btn2)
+        {
+            InitializeComponent();
+            btnN1 = btn1;
+            btnN2 = btn2;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            btnN1.IsEnabled = true;
+            btnN2.IsEnabled = true;
+        }
+    }
+}
